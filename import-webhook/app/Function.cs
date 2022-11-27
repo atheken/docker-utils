98cd73ca082b711de26817e3cb07cf80a469c5e7
Andrew Theken <andrew@attr.io>
2022-11-26 21:01:39 -0500

    Build a function using the lambda container.

--- import-webhook/app/Function.cs
+++ import-webhook/app/Function.cs
@@ -1,10 +1,16 @@
 ﻿using PostmarkDotNet.Model;
 
+[assembly:Amazon.Lambda.Core.LambdaSerializerAttribute(
+    typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
 namespace app;
+
 public class Function
 {
-    public void Handler(InboundMessage message)
+    public record class Person(string name, int age);
+    
+    public async Task<Person> Handler(InboundMessage message)
     {
-        
+        return await Task.FromResult(new Person("asdf", 42));
     }
 }
