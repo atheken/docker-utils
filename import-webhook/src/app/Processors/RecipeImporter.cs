34bce33888a850dae45330b17e8eded017962b89
Andrew Theken <andrew@attr.io>
2022-11-28 17:49:39 -0500

    Customize docker build.

--- import-webhook/src/app/Processors/RecipeImporter.cs
+++ import-webhook/src/app/Processors/RecipeImporter.cs
@@ -17,7 +17,7 @@ public class RecipeImporter
     public RecipeImporter(ILogger logger, HttpClient? client = null,
         string? baseUrl = null, string? apiKey = null)
     {
-        _failedDir = Environment.GetEnvironmentVariable("APP_DATA_DIR") + "/failed";
+        _failedDir =  (Environment.GetEnvironmentVariable("APP_DATA_DIR") ?? Directory.CreateTempSubdirectory().Name) + "/failed";
         Directory.CreateDirectory(_failedDir);
         
         _baseUrl = baseUrl ?? Environment.GetEnvironmentVariable("MEALIE_BASE_URL");
