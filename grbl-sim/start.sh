5662e8e722fdfc355bd286751b61c5641f7e79a2
Andrew Theken <andrew@attr.io>
2023-03-01 15:02:47 -0500

    Add more customization of the grbl-sim

--- grbl-sim/start.sh
+++ grbl-sim/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Starting socket on '127.0.0.1:9600'..."
-exec socat -d -d TCP-LISTEN:9600 "EXEC:'./grbl-sim $GRBL_REALTIME_FACTOR -c\'$GRBL_COMMENT\' -s /dev/stdout -b /dev/stdout',pty,raw,echo=0"
+exec socat -d -d TCP-LISTEN:9600 "EXEC:'./grbl-sim -t $GRBL_REALTIME_FACTOR -c\'$GRBL_COMMENT\' $GRBL_ARGS -s /dev/stdout -b /dev/stdout',pty,raw,echo=0"
