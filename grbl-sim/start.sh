d84bf973dd63df49e0c7bad11ca17280e4e106b5
Andrew Theken <andrew@attr.io>
2023-03-01 14:43:41 -0500

    Allow configuration of the GRBL comment prefix, as well as the realtime factor for grbl-sim

--- grbl-sim/start.sh
+++ grbl-sim/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Starting socket on '127.0.0.1:9600'..."
-exec socat -d -d TCP-LISTEN:9600 "EXEC:'./grbl-sim 1.0 -s /dev/stdout -b /dev/stdout',pty,raw,echo=0"
+exec socat -d -d TCP-LISTEN:9600 "EXEC:'./grbl-sim $GRBL_REALTIME_FACTOR -c\'$GRBL_COMMENT\' -s /dev/stdout -b /dev/stdout',pty,raw,echo=0"
